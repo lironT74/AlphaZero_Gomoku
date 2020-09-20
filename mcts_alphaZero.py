added shutter size statistics within mantches and fixed shutter size calculation

mcts_alphaZero.py
@@ -196,6 +196,7 @@ class MCTSPlayer(object):
         self.name = name
 
         self.input_plains_num = kwargs.get("input_plains_num", 4)  # default does receive last turn
+        self.no_playouts = kwargs.get("no_playouts", False)
 
     def set_player_ind(self, p):
         self.player = p
@@ -212,25 +213,29 @@ class MCTSPlayer(object):
         display = kwargs.get('display', False)
 
         # the pi vector returned by MCTS as in the alphaGo Zero paper
+
         move_probs = np.zeros(board.width * board.height)
+
         if len(sensible_moves) > 0:
-
-            acts_mcts, probas_mcts, visits_mcts = self.mcts.get_move_probs(board, temp, return_visits=True)
-
-            # Check if there is a last move indicated - in the start of empty board
-            # game there is no last move.
-
-            if np.sum(board.current_state(last_move=True)[2]) == 1:
-                y_last_move = 6 - np.where(board.current_state(last_move=True)[2] == 1)[0][0]
-                x_last_move = string.ascii_lowercase[np.where(board.current_state(last_move=True)[2] == 1)[1][0]]
-                last_move = f"last move: {x_last_move}{y_last_move}"
-            else:
-                last_move = "No last move"
 
 
             acts_policy, probas_policy = zip(*self.mcts._policy(board)[0])
 
+            # AlphaZero gives some probability to locations that are not available for some reason
+            probas_policy = probas_policy / np.sum(probas_policy)
+
+            if not self.no_playouts:
+                acts_mcts, probas_mcts, visits_mcts = self.mcts.get_move_probs(board, temp, return_visits=True)
+
+            else:
+                acts_mcts, probas_mcts = acts_policy, probas_policy
+                visits_mcts = 0
+
             move_probs[list(acts_mcts)] = probas_mcts
+
+            # Check if there is a last move indicated - in the start of empty board
+            # game there is no last move.
+
 
             if self._is_selfplay:
                 # add Dirichlet Noise for exploration (needed for
@@ -245,18 +250,47 @@ class MCTSPlayer(object):
                 # with the default temp=1e-3, it is almost equivalent
                 # to choosing the move with the highest prob
                 move = np.random.choice(acts_mcts, p=probas_mcts)
+
+                # for act, prob in zip(acts_mcts, probas_mcts):
+                #     y_cur_act = act // board.width + 1
+                #     x_cur_act = string.ascii_lowercase[act % board.width]
+                #     cur_move = f"{x_cur_act}{y_cur_act}"
+                #     print(f"{(cur_move, prob)}")
+
+
+
                 # reset the root node
                 self.mcts.update_with_move(-1)
             #                location = board.move_to_location(move)
             #                print("AI move: %d,%d\n" % (location[0], location[1]))
 
 
-            last_moves_data = kwargs.get("last_move_data", [])
-            row = board.width - 1 - move // board.width
-            col = move % board.width
+            y_cur_move = move // board.width + 1
+            x_cur_move = string.ascii_lowercase[move % board.width]
+            cur_move = f"{x_cur_move}{y_cur_move}"
 
-            shutter_size = get_shutter_size(last_move_data=last_moves_data, current_move=(row, col))
 
+            if np.sum(board.current_state(last_move=True)[2]) == 1:
+                y_last_move = board.width - np.where(board.current_state(last_move=True)[2] == 1)[0][0]
+                x_last_move = string.ascii_lowercase[np.where(board.current_state(last_move=True)[2] == 1)[1][0]]
+                last_move = f"last move: {x_last_move}{y_last_move}"
+
+                row_last = y_last_move - 1
+                col_last = np.where(board.current_state(last_move=True)[2] == 1)[1][0]
+
+                row_cur = board.height - 1 - move // board.width
+                col_cur = move % board.width
+
+                shutter_size = get_shutter_size(last_move=(row_last, col_last), board = board, cur_move=(row_cur, col_cur))
+
+            else:
+                last_move = "No last move"
+                shutter_size = -1
+
+
+
+
+            # print(cur_move)
 
             if display:
                 self.create_probas_heatmap(acts_policy=acts_policy,
@@ -269,6 +303,7 @@ class MCTSPlayer(object):
                                            board=board,
                                            last_move=last_move,
                                            shutter_size=shutter_size,
+                                           cur_move=cur_move,
                                            display=True)
 
             if return_fig:
@@ -281,6 +316,7 @@ class MCTSPlayer(object):
                                                  height=board.height,
                                                  board=board,
                                                  last_move=last_move,
+                                                 cur_move=cur_move,
                                                  shutter_size=shutter_size)
 
                 if return_prob:
@@ -301,11 +337,12 @@ class MCTSPlayer(object):
         return str(self.name) + " {}".format(self.player)
 
     def create_probas_heatmap(self, acts_policy, probas_policy, acts_mcts, probas_mcts, visits_mcts, width, height,
-                              board, last_move, shutter_size=-1, display=False):
+                              board, last_move, cur_move, shutter_size=-1, display=False):
+
+
 
         if not display:
             mpl.use('Agg')
-
 
 
         if hasattr(self, 'player'):
@@ -339,76 +376,110 @@ class MCTSPlayer(object):
             shutter_str = ""
 
 
+
         move_probs_policy = np.zeros(width * height)
         move_probs_policy[list(acts_policy)] = probas_policy
         move_probs_policy = move_probs_policy.reshape(width, height)
         move_probs_policy = np.flipud(move_probs_policy)
-        move_probs_policy = np.round_(move_probs_policy, decimals=3)
+        move_probs_policy = np.round_(move_probs_policy, decimals=4)
+
+        if visits_mcts != 0:
+
+            move_probs_mcts = np.zeros(width * height)
+            move_probs_mcts[list(acts_mcts)] = probas_mcts
+            move_probs_mcts = move_probs_mcts.reshape(width, height)
+            move_probs_mcts = np.flipud(move_probs_mcts)
+            move_probs_mcts = np.round_(move_probs_mcts, decimals=3)
 
 
-        normalized_visits = np.zeros(width * height)
-        visits_mcts = visits_mcts / np.sum(visits_mcts)
-        normalized_visits[list(acts_mcts)] = visits_mcts
-        normalized_visits = normalized_visits.reshape(width, height)
-        normalized_visits = np.flipud(normalized_visits)
-        normalized_visits = np.round_(normalized_visits, decimals=3)
+            normalized_visits = np.zeros(width * height)
+            visits_mcts = visits_mcts / np.sum(visits_mcts)
+            normalized_visits[list(acts_mcts)] = visits_mcts
+            normalized_visits = normalized_visits.reshape(width, height)
+            normalized_visits = np.flipud(normalized_visits)
+            normalized_visits = np.round_(normalized_visits, decimals=3)
 
 
-        move_probs_mcts = np.zeros(width * height)
-        move_probs_mcts[list(acts_mcts)] = probas_mcts
-        move_probs_mcts = move_probs_mcts.reshape(width, height)
-        move_probs_mcts = np.flipud(move_probs_mcts)
-        move_probs_mcts = np.round_(move_probs_mcts, decimals=3)
+            titles = ["Probas of the policy value fn", "Normalized visit counts of MCTS",
+                      f"Probas of the MCTS. played: {cur_move}"]
+
+            distributions = [move_probs_policy, normalized_visits, move_probs_mcts]
 
 
-        titles = ["Probas of the policy value fn", "Normalized visit counts of MCTS", "Probas of the MCTS"]
-        distributions = [move_probs_policy, normalized_visits, move_probs_mcts]
+            fontsize = 19
+            fig = plt.figure(constrained_layout=False)
+            fig.set_size_inches(45, 15)
+
+            grid = fig.add_gridspec(nrows=3, ncols=7, height_ratios=[40, 2, 0.1], width_ratios=[2, 15, 1, 15, 1, 15, 2])
+
+            sm = plt.cm.ScalarMappable(cmap=cmap, norm=plt.Normalize(vmin=0, vmax=1))
+            fig.suptitle(f"\nModel: {self.name} (plays: {my_marker}, "
+                         f"{last_move}{shutter_str})\nMCTS playouts: {self.mcts._n_playout}\n", fontsize=fontsize + 10)
+
+            for i, (title, dist) in enumerate(zip(titles, distributions)):
+
+                ax = fig.add_subplot(grid[0, i * 2 + 1])
+
+                im = ax.imshow(dist, cmap=cmap, norm=plt.Normalize(vmin=0, vmax=1))
+
+                ax.set_xticks(np.arange(len(x_axis)))
+                ax.set_yticks(np.arange(len(y_axis)))
+                ax.set_xticklabels(x_axis, fontsize=fontsize)
+                ax.set_yticklabels(y_axis, fontsize=fontsize)
+
+                plt.setp(ax.get_xticklabels(), ha="right", rotation_mode="anchor")
+                for i in range(len(y_axis)):
+                    for j in range(len(x_axis)):
+                        color = "black" if dist[i, j] < 0.55 else "white"
+                        text = ax.text(j, i, "X" if x_positions[i, j] == 1 else (
+                            "O" if o_positions[i, j] == 1 else dist[i, j]),
+                                       ha="center", va="center", color=color, fontsize=fontsize + 3)
+
+                ax.set_title(title, fontsize=fontsize + 5)
+
+            cbar_ax = fig.add_subplot(grid[1, 1:-1])
+            fig.colorbar(sm, cax=cbar_ax, orientation="horizontal").ax.tick_params(labelsize=fontsize + 2)
+
+        else:
 
 
-        fontsize = 19
-        fig = plt.figure(constrained_layout=False)
-        fig.set_size_inches(45, 15)
+            fontsize = 38
+            fig, ax = plt.subplots(tight_layout=False, figsize = (25, 27))
 
-        grid = fig.add_gridspec(nrows=3, ncols=7, height_ratios = [40,2,0.1], width_ratios = [2,15,1,15,1,15,2])
+            sm = plt.cm.ScalarMappable(cmap=cmap, norm=plt.Normalize(vmin=0, vmax=1))
+            fig.suptitle(f"Model: {self.name}, using the policy value function.\nPlays: {my_marker}, "
+                         f"{last_move}{shutter_str}\n Chosen move: {cur_move}", fontsize=fontsize + 10)
 
-        sm = plt.cm.ScalarMappable(cmap=cmap, norm=plt.Normalize(vmin=0, vmax=1))
-        fig.suptitle(f"\nModel: {self.name} (plays: {my_marker}, "
-                     f"{last_move}{shutter_str})\nMCTS playouts: {self.mcts._n_playout}\n", fontsize=fontsize + 10)
 
-        for i, (title, dist) in enumerate(zip(titles, distributions)):
+            im = ax.imshow(move_probs_policy, cmap=cmap, norm=plt.Normalize(vmin=0, vmax=1))
 
-            ax = fig.add_subplot(grid[0, i*2+1])
+            divider = make_axes_locatable(ax)
+            cax = divider.new_vertical(size="5%", pad=0.7, pack_start=True)
+            fig.add_axes(cax)
+            fig.colorbar(im, cax=cax, orientation="horizontal").ax.tick_params(labelsize=fontsize+2)
 
-            im = ax.imshow(dist, cmap=cmap, norm=plt.Normalize(vmin=0, vmax=1))
 
             ax.set_xticks(np.arange(len(x_axis)))
             ax.set_yticks(np.arange(len(y_axis)))
             ax.set_xticklabels(x_axis, fontsize=fontsize)
             ax.set_yticklabels(y_axis, fontsize=fontsize)
 
-
             plt.setp(ax.get_xticklabels(), ha="right", rotation_mode="anchor")
             for i in range(len(y_axis)):
                 for j in range(len(x_axis)):
-                    color = "black" if dist[i,j] < 0.55 else "white"
+                    color = "black" if move_probs_policy[i, j] < 0.55 else "white"
                     text = ax.text(j, i, "X" if x_positions[i, j] == 1 else (
-                        "O" if o_positions[i, j] == 1 else dist[i, j]),
-                                    ha="center", va="center", color=color, fontsize=fontsize+3)
-
-            ax.set_title(title, fontsize=fontsize + 5)
+                        "O" if o_positions[i, j] == 1 else move_probs_policy[i, j]),
+                                   ha="center", va="center", color=color, fontsize=fontsize + 3)
 
 
-        cbar_ax = fig.add_subplot(grid[1, 1:-1])
-        fig.colorbar(sm, cax=cbar_ax, orientation="horizontal").ax.tick_params(labelsize=fontsize + 2)
-
-        # grid.tight_layout(fig)
+        fig.subplots_adjust(left = 0.083, right=1-0.083)
 
         if display:
             plt.show()
             return
 
         else:
-            fig.subplots_adjust(top=0.88)
             buf = io.BytesIO()
             plt.savefig(buf, format='png')
             buf.seek(0)

model_matches.py
@@ -1,9 +1,10 @@
 from __future__ import print_function
 from multiprocessing import Pool
-from game import Game
 from mcts_alphaZero import MCTSPlayer
 from Game_boards_and_aux import *
 from scipy.special import comb
+import pandas as pd
+import os
 
 def compare_all_models(models_list, width=6, height=6, n=4, open_path_threshold=-1, n_playout=400):
 
@@ -22,17 +23,17 @@ def compare_all_models(models_list, width=6, height=6, n=4, open_path_threshold=
 
 def compare_two_models(model1, model2, width, height, n, open_path_threshold, n_playout):
 
-    path1, name1, plains1 = model1
-    path2, name2, plains2 = model2
-
-
+    path1, name1, plains1, no_playouts1 = model1
+    path2, name2, plains2, no_playouts2 = model2
 
     best_policy_1 = PolicyValueNet(width, height, model_file=path1, input_plains_num=plains1)
-    mcts_player_1 = MCTSPlayer(best_policy_1.policy_value_fn, c_puct=5, n_playout=n_playout, name=name1, input_plains_num=plains1)
+    mcts_player_1 = MCTSPlayer(best_policy_1.policy_value_fn, c_puct=5, n_playout=n_playout, no_playouts = no_playouts1, name=name1, input_plains_num=plains1)
 
     best_policy_2 = PolicyValueNet(width, height, model_file=path2, input_plains_num=plains2)
-    mcts_player_2 = MCTSPlayer(best_policy_2.policy_value_fn, c_puct=5, n_playout=n_playout, name=name2, input_plains_num=plains2)
+    mcts_player_2 = MCTSPlayer(best_policy_2.policy_value_fn, c_puct=5, n_playout=n_playout, no_playouts = no_playouts2, name=name2, input_plains_num=plains2)
 
+    main_dir = "matches/matches - new selected models" if not no_playouts1 and not no_playouts2 \
+        else "matches/no MCTS matches"
 
     for board_state, board_name, p1, p2, alternative_p1, alternative_p2 in PAPER_TRUNCATED_BOARDS:
 
@@ -48,7 +49,8 @@ def compare_two_models(model1, model2, width, height, n, open_path_threshold, n_
                       correct_move_p1=p1,
                       correct_move_p2=p2,
                       start_player=2,
-                      open_path_threshold=open_path_threshold)
+                      open_path_threshold=open_path_threshold,
+                      main_dir=main_dir)
 
         if plains1+plains2 >= 7:
 
@@ -64,7 +66,8 @@ def compare_two_models(model1, model2, width, height, n, open_path_threshold, n_
                           correct_move_p1=p1,
                           correct_move_p2=p2,
                           start_player=2,
-                          open_path_threshold=open_path_threshold)
+                          open_path_threshold=open_path_threshold,
+                          main_dir=main_dir)
 
 
         if plains1+plains2 == 8:
@@ -81,7 +84,8 @@ def compare_two_models(model1, model2, width, height, n, open_path_threshold, n_
                           correct_move_p1=p1,
                           correct_move_p2=p2,
                           start_player=2,
-                          open_path_threshold=open_path_threshold)
+                          open_path_threshold=open_path_threshold,
+                          main_dir=main_dir)
 
             save_game_res(width=width,
                           height=height,
@@ -95,7 +99,8 @@ def compare_two_models(model1, model2, width, height, n, open_path_threshold, n_
                           correct_move_p1=p1,
                           correct_move_p2=p2,
                           start_player=2,
-                          open_path_threshold=open_path_threshold)
+                          open_path_threshold=open_path_threshold,
+                          main_dir=main_dir)
 
     for board_state, board_name, p1, p2, alternative_p1, alternative_p2 in PAPER_FULL_BOARDS:
         save_game_res(width=width,
@@ -110,7 +115,8 @@ def compare_two_models(model1, model2, width, height, n, open_path_threshold, n_
                       correct_move_p1=p1,
                       correct_move_p2=p2,
                       start_player=2,
-                      open_path_threshold=open_path_threshold)
+                      open_path_threshold=open_path_threshold,
+                      main_dir=main_dir)
 
     board_state, board_name, p1, p2, alternative_p1, alternative_p2 = EMPTY_BOARD
 
@@ -126,12 +132,18 @@ def compare_two_models(model1, model2, width, height, n, open_path_threshold, n_
                   correct_move_p1=p1,
                   correct_move_p2=p2,
                   start_player=1,
-                  open_path_threshold=open_path_threshold)
+                  open_path_threshold=open_path_threshold,
+                  main_dir=main_dir)
 
 
-def save_game_res(width, height, n, board_state, board_name, mcts_player_1, mcts_player_2, last_move_p1, last_move_p2, correct_move_p1, correct_move_p2, start_player, open_path_threshold):
+def save_game_res(width, height, n, board_state, board_name, mcts_player_1, mcts_player_2, last_move_p1,
+                  last_move_p2, correct_move_p1, correct_move_p2, start_player, open_path_threshold, main_dir):
+
+
     i_board1, board1 = initialize_board_without_init_call(width, height, n, input_board=board_state, open_path_threshold=open_path_threshold)
     game1 = Game(board1)
+
+
     game1.start_play(player1=mcts_player_1, player2=mcts_player_2,
                      start_player=start_player,
                      is_shown=0,
@@ -141,7 +153,8 @@ def save_game_res(width, height, n, board_state, board_name, mcts_player_1, mcts
                      correct_move_p1=correct_move_p1,
                      correct_move_p2=correct_move_p2,
                      savefig=1,
-                     board_name=board_name)
+                     board_name=board_name,
+                     main_dir=main_dir)
 
     i_board2, board2 = initialize_board_without_init_call(width, height, n, input_board=board_state, open_path_threshold=open_path_threshold)
     game2 = Game(board2)
@@ -154,14 +167,230 @@ def save_game_res(width, height, n, board_state, board_name, mcts_player_1, mcts
                     correct_move_p1=correct_move_p1,
                     correct_move_p2=correct_move_p2,
                     savefig=1,
-                    board_name=board_name)
+                    board_name=board_name,
+                    main_dir=main_dir)
+
+
+
+def compare_all_models_statistics(models_list, width=6, height=6, n=4, open_path_threshold=-1, n_playout=400, num_games=100):
+
+    jobs = []
+    for i in range(len(models_list)):
+        for j in range(i+1, len(models_list)):
+            jobs.append((models_list[i], models_list[j], width, height, n, open_path_threshold, n_playout, num_games))
+
+
+    with Pool(int(comb(len(models_list), 2))) as pool:
+        print(f"Using {pool._processes} workers. There are {len(jobs)} jobs: \n")
+
+        pool.starmap(collect_statistics_two_models, jobs)
+        pool.close()
+        pool.join()
+
+
+
+def collect_statistics_two_models(model1, model2, width, height, n, open_path_threshold, n_playout, num_games):
+
+
+    path1, name1, plains1, no_playouts1 = model1
+    path2, name2, plains2, no_playouts2 = model2
+
+    best_policy_1 = PolicyValueNet(width, height, model_file=path1, input_plains_num=plains1)
+    mcts_player_1 = MCTSPlayer(best_policy_1.policy_value_fn, c_puct=5, n_playout=n_playout, no_playouts=no_playouts1,
+                               name=name1, input_plains_num=plains1)
+
+    best_policy_2 = PolicyValueNet(width, height, model_file=path2, input_plains_num=plains2)
+    mcts_player_2 = MCTSPlayer(best_policy_2.policy_value_fn, c_puct=5, n_playout=n_playout, no_playouts=no_playouts2,
+                               name=name2, input_plains_num=plains2)
+
+    for board_state, board_name, p1, p2, alternative_p1, alternative_p2 in PAPER_TRUNCATED_BOARDS:
+
+        save_games_statistics(width=width,
+                      height=height,
+                      n=n,
+                      board_state=board_state,
+                      board_name=board_name,
+                      mcts_player_1=mcts_player_1,
+                      mcts_player_2=mcts_player_2,
+                      last_move_p1=p1,
+                      last_move_p2=p2,
+                      correct_move_p1=p1,
+                      correct_move_p2=p2,
+                      start_player=2,
+                      open_path_threshold=open_path_threshold, num_games=num_games)
+
+        if plains1 + plains2 >= 7:
+            save_games_statistics(width=width,
+                          height=height,
+                          n=n,
+                          board_state=board_state,
+                          board_name=board_name,
+                          mcts_player_1=mcts_player_1,
+                          mcts_player_2=mcts_player_2,
+                          last_move_p1=alternative_p1,
+                          last_move_p2=alternative_p2,
+                          correct_move_p1=p1,
+                          correct_move_p2=p2,
+                          start_player=2,
+                          open_path_threshold=open_path_threshold, num_games=num_games)
+
+        if plains1 + plains2 == 8:
+            save_games_statistics(width=width,
+                          height=height,
+                          n=n,
+                          board_state=board_state,
+                          board_name=board_name,
+                          mcts_player_1=mcts_player_1,
+                          mcts_player_2=mcts_player_2,
+                          last_move_p1=p1,
+                          last_move_p2=alternative_p2,
+                          correct_move_p1=p1,
+                          correct_move_p2=p2,
+                          start_player=2,
+                          open_path_threshold=open_path_threshold, num_games=num_games)
+
+            save_games_statistics(width=width,
+                          height=height,
+                          n=n,
+                          board_state=board_state,
+                          board_name=board_name,
+                          mcts_player_1=mcts_player_1,
+                          mcts_player_2=mcts_player_2,
+                          last_move_p1=alternative_p1,
+                          last_move_p2=p2,
+                          correct_move_p1=p1,
+                          correct_move_p2=p2,
+                          start_player=2,
+                          open_path_threshold=open_path_threshold, num_games=num_games)
+
+    for board_state, board_name, p1, p2, alternative_p1, alternative_p2 in PAPER_FULL_BOARDS:
+        save_games_statistics(width=width,
+                      height=height,
+                      n=n,
+                      board_state=board_state,
+                      board_name=board_name,
+                      mcts_player_1=mcts_player_1,
+                      mcts_player_2=mcts_player_2,
+                      last_move_p1=p1,
+                      last_move_p2=p2,
+                      correct_move_p1=p1,
+                      correct_move_p2=p2,
+                      start_player=2,
+                      open_path_threshold=open_path_threshold, num_games=num_games)
+
+    board_state, board_name, p1, p2, alternative_p1, alternative_p2 = EMPTY_BOARD
+
+    save_games_statistics(width=width,
+                  height=height,
+                  n=n,
+                  board_state=board_state,
+                  board_name=board_name,
+                  mcts_player_1=mcts_player_1,
+                  mcts_player_2=mcts_player_2,
+                  last_move_p1=p1,
+                  last_move_p2=p2,
+                  correct_move_p1=p1,
+                  correct_move_p2=p2,
+                  start_player=1,
+                  open_path_threshold=open_path_threshold, num_games=num_games)
+
+
+
+def save_games_statistics(width, height, n, board_state, board_name, mcts_player_1,
+                          mcts_player_2, last_move_p1, last_move_p2, correct_move_p1,
+                          correct_move_p2, start_player, open_path_threshold, num_games):
+
+    i_board1, board1 = initialize_board_without_init_call(width, height, n, input_board=board_state,
+                                                          open_path_threshold=open_path_threshold)
+    game1 = Game(board1)
+
+
+    wins = []
+
+    shutters = {mcts_player_1.name: [], mcts_player_2.name: []}
+
+    for i in range(num_games):
+
+        current_players = {2-i%2: mcts_player_1, 1 + i%2: mcts_player_2}
+
+        winner, game_length, shutter_sizes = game1.start_play(player1=current_players[1],
+                                                              player2=current_players[2],
+                                                             start_player=start_player,
+                                                             is_shown=0,
+                                                             start_board=i_board1,
+                                                             last_move_p1=last_move_p1,
+                                                             last_move_p2=last_move_p2,
+                                                             correct_move_p1=correct_move_p1,
+                                                             correct_move_p2=correct_move_p2,
+                                                             savefig=0,
+                                                             board_name=board_name,
+                                                             return_statistics=1)
+
+        if winner != -1:
+            wins.append(current_players[winner].name)
+
+
+        plays = range(1, game_length + 1, 1)
+        start_player_range = [(index, shutter) for index, shutter in zip(plays[0::2], shutter_sizes[start_player]) if
+                              shutter != -1]
+        second_player_range = [(index, shutter) for index, shutter in zip(plays[1::2], shutter_sizes[3 - start_player])
+                               if shutter != -1]
+
+
+        shutters[current_players[start_player].name].extend([x[1] for x in start_player_range])
+        shutters[current_players[3 - start_player].name].extend([x[1] for x in second_player_range])
+
+
+
+    columns = [f"average shutter size", f"no. of plays which had shutter != -1",  "no. wins"]
+
+    index = [f"{mcts_player_1.name}", f"{mcts_player_2.name}"]
+
+    np_results = np.array([[np.average(shutters[mcts_player_1.name]), len(shutters[mcts_player_1.name]), wins.count(mcts_player_1.name)],
+                           [np.average(shutters[mcts_player_2.name]), len(shutters[mcts_player_2.name]), wins.count(mcts_player_2.name)]])
+
+    df = pd.DataFrame(np_results, index=index, columns=columns)
+
+    print(df.to_string())
+
+    if mcts_player_1.no_playouts or mcts_player_2.no_playouts:
+        path = f"/home/lirontyomkin/AlphaZero_Gomoku/matches/statistics/no MCTS/{mcts_player_1.name} vs {mcts_player_2.name}/"
+    else:
+        path = f"/home/lirontyomkin/AlphaZero_Gomoku/matches/statistics/{mcts_player_1.name} vs {mcts_player_2.name}/"
+
+    if not os.path.exists(f"{path}{board_name}/"):
+        os.makedirs(f"{path}{board_name}/")
+
+    df.to_excel(f"{path}{board_name}/{num_games} games statistics.xlsx", index=True, header=True)
+
+    f = open(f"{path}{mcts_player_1.name} vs {mcts_player_2.name} {num_games} games results.txt", "a")
+    f.write(f"----------> {board_name} <----------\n")
+    f.write(df.to_string())
+    f.write('\n\n')
+    f.close()
 
 
 if __name__ == '__main__':
 
-    v7 = ('/home/lirontyomkin/AlphaZero_Gomoku/models/pt_6_6_4_p3_v7/current_policy_2100.model', 'pt_6_6_4_p3_v7_2100', 3)
-    v9 = ('/home/lirontyomkin/AlphaZero_Gomoku/models/pt_6_6_4_p3_v9/current_policy_1350.model', 'pt_6_6_4_p3_v9_1350', 3)
-    v10 = ('/home/lirontyomkin/AlphaZero_Gomoku/models/pt_6_6_4_p4_v10/current_policy_1150.model', 'pt_6_6_4_p4_v10_1150', 4)
+    v7 = ('/home/lirontyomkin/AlphaZero_Gomoku/models/pt_6_6_4_p3_v7/current_policy_2100.model', 'pt_6_6_4_p3_v7_2100', 3, False)
+    v9 = ('/home/lirontyomkin/AlphaZero_Gomoku/models/pt_6_6_4_p3_v9/current_policy_1350.model', 'pt_6_6_4_p3_v9_1350', 3, False)
+    v10 = ('/home/lirontyomkin/AlphaZero_Gomoku/models/pt_6_6_4_p4_v10/current_policy_1150.model', 'pt_6_6_4_p4_v10_1150', 4, False)
+
+    models = [v7, v9, v10]
+    compare_all_models(models)
+
+
+    v9 = ( '/home/lirontyomkin/AlphaZero_Gomoku/models/pt_6_6_4_p3_v9/current_policy_1500.model',
+           'pt_6_6_4_p3_v9_1500', 3,True)
+
+    v10 = ('/home/lirontyomkin/AlphaZero_Gomoku/models/pt_6_6_4_p4_v10/current_policy_1500.model',
+           'pt_6_6_4_p4_v10_1500', 4, True)
+
+    v7 = ('/home/lirontyomkin/AlphaZero_Gomoku/models/pt_6_6_4_p3_v7/current_policy_1500.model',
+          'pt_6_6_4_p3_v7_1500', 3, True)
+
     models = [v7, v9, v10]
 
     compare_all_models(models)
+    # compare_all_models_statistics(models, num_games=10000)
+